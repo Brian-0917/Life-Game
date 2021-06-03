dac4a4def9c06ae01e31193dc818d08791f35942
Brian-0917 <wade03201019@mail.com>
2021-06-03 20:57:21 +0800

    rwd-1

---
+++ JS/index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var tlmenu = new TimelineMax();
+
+    var top_mv = TweenMax.to('.but_top', .3, {
+        y: '11px',
+        rotation: 45
+    })
+    var middle_mv = TweenMax.to('.middle', .1, {
+        opacity: 0,
+
+    })
+    var bottom_mv = TweenMax.to('.but_bottom', .3, {
+        rotation: -45,
+        y: '-11px'
+    })
+
+    // });
+    // timelinemax 同時執行動畫
+    tlmenu.add([top_mv, middle_mv, bottom_mv])
+
+    tlmenu.to('.items_other', .1, {
+        opacity: 1,
+    }).staggerTo('.items_other li', .2, {
+        opacity: 1
+    }, .4)
+
+    // 控制
+    tlmenu.stop();
+
+    var flag = 0;
+    document.getElementById('nav').onclick = function() {
+        if (flag == 0) {
+            tlmenu.play();
+            flag = 1;
+        } else {
+            tlmenu.reverse();
+            flag = 0;
+        }
+
+    }
+
+});
