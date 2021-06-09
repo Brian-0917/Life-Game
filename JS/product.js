fcbde493ca0994534a9b6ac7bfc95d9ca77cb1e6
Brian-0917 <wade03201019@mail.com>
2021-06-10 04:50:16 +0800

    order_rwd1

--- JS/product.js
+++ JS/product.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var tlrank = new TimelineMax();
+    // var tlrank = new TimelineMax();
 
-    tlrank.to('.rank', .2, { display: 'block' }, {
+    // tlrank.to('.rank', .2, { display: 'block' }, {
 
-    }).to('.rank', .5, {
-        opacity: 1
-    })
+    // }).to('.rank', .5, {
+    //     opacity: 1
+    // })
 
-    tlrank.stop();
+    // tlrank.stop();
 
 
     var tltag = new TimelineMax();
-    tltag.to('.slmv1', .5, {
+    tltag.to('.slmv1', .5, { display: 'block' }, {
+
+    }).to('.slmv1', .5, {
         right: 0
     })
 
@@ -21,16 +23,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     var ck = 0;
-    var rank_block = document.querySelector("div.but_title");
-    rank_block.addEventListener('click', function() {
-        if (ck == 0) {
-            tlrank.play();
-            ck = 1;
-        } else {
-            tlrank.reverse();
-            ck = 0;
-        }
-    });
+    // var rank_block = document.querySelector("div.but_title");
+    // rank_block.addEventListener('click', function() {
+    //     if (ck == 0) {
+    //         tlrank.play();
+    //         ck = 1;
+    //     } else {
+    //         tlrank.reverse();
+    //         ck = 0;
+    //     }
+    // });
     var select_block = document.querySelector("div.selecter");
     select_block.addEventListener('click', function() {
         if (ck == 0) {
